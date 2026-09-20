66bc2f2a713dde5df18b6bc1375ea0ca13d69b11
Henil <hiraparahenil2005@gmail.com>
2026-09-20 12:46:43 +0530

    Fix MyOrders import path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ import PlaceOrder from './pages/placeOrder/PlaceOrder'
 import Footer from './components/Footer/Footer'
 import LoginPopUp from './components/LoginPopUp/LoginPopUp'
 import Verify from './pages/verify/Verify'
-import MyOrders from './pages/MyOrders/MyOrders'
+import MyOrders from './pages/myOrder/myOrder';
 import { StoreContext } from './context/StoreContext'
 
 const App = () => {
